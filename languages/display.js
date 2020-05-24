97540b374ab6a1bfb11e7f50a57fcca1fc9707d5
CLASHERBROs <parigames@gmail.com>
2020-05-24 18:04:31 +0530

    Added animations

--- languages/display.js
+++ languages/display.js
@@ -7,13 +7,14 @@ buttons.each(function( index){
         
 
         if($(videos[index]).attr("bool")=="true"){
-               
-               $(videos[index]).css("display","none");
+               $(videos[index]).slideUp();
+            //    $(videos[index]).css("display","none");
                $(videos[index]).attr("bool","false");
        
            }
            else{
-            $(videos[index]).css("display","block");
+            $(videos[index]).slideDown();
+            // $(videos[index]).css("display","block");
             $(videos[index]).attr("bool","true");
              
            }
